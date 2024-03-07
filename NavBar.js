3674b0c2afe09e76ef0d4601914fef91c890570a
SaudDuas <83941535+SaudDuas@users.noreply.github.com>
2024-03-07 23:47:34 +0300

    Add files via upload

---
+++ NavBar.js
@@ -0,0 +1,24 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/timer">Timer</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
